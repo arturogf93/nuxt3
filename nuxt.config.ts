488a03d55b95ce4c910be8ed9822b256f3e8f9b6
Arturo Gonzalez <arturogf93@gmail.com>
2022-03-13 11:39:22 -0600

    nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,5 +7,8 @@ export default defineNuxtConfig({
     'primevue/resources/primevue.css',
     'primeicons/primeicons.css',
 		'@/assets/css/style.css'
-	]
+	],
+	build: {
+    transpile: ['primevue']
+	}
 })
