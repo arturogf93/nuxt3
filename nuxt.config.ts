cc33b17ec634dc4465e5bb45b5f6161ace287fa4
Arturo Gonzalez <arturogf93@gmail.com>
2022-03-13 11:46:32 -0600

    layout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,10 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
 	css: [
-		'primevue/resources/themes/saga-blue/theme.css',
+		'primevue/resources/themes/lara-light-blue/theme.css',
     'primevue/resources/primevue.css',
     'primeicons/primeicons.css',
+		'primeflex/primeflex.css',
 		'@/assets/css/style.css'
 	],
 	build: {
