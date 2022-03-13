c7e77027cef61994b974fa98e92c02fc320f6e5e
Arturo Gonzalez <arturogf93@gmail.com>
2022-03-13 12:28:38 -0600

    route param

---
+++ pages/pokemon/[name].vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useRoute } from 'vue-router'
+const route = useRoute()
+const { data } = await useFetch(`https://pokeapi.co/api/v2/pokemon/${route.params.name}`)
+</script>
+<template>
+	<Html>
+		<Head>
+			<Title>{{ data.name }}</Title>
+			<Meta name="description" :content="`My page's ${data.name} description`" />
+			<Meta property="og:image" :content="data.sprites.other['official-artwork'].front_default" />
+		</Head>
+	</Html>
+	<div>
+		pokemon:
+		<code>
+			{{ data }}
+		</code>
+	</div>
+</template>
