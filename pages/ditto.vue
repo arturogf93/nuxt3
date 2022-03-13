5b39f9c30bd70ce1d9f5856e1ddeab01229989f4
Arturo Gonzalez <arturogf93@gmail.com>
2022-03-13 12:11:41 -0600

    fetch

---
+++ pages/ditto.vue
@@ -0,0 +1,19 @@
+<script setup>
+const { data } = await useFetch('https://pokeapi.co/api/v2/pokemon/ditto')
+</script>
+<template>
+	<Html>
+		<Head>
+			<Title>{{ data.name }}</Title>
+			<Meta name="description" :content="`My page's ${data.name} description`" />
+			<Link rel="preload" href="/test.txt" as="script" />
+			<Style type="text/css" :children="styleString" />
+		</Head>
+	</Html>
+	<div>
+		pokemon:
+		<code>
+			{{ data }}
+		</code>
+	</div>
+</template>
