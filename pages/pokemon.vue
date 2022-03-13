c00fdbdb90b8bacc7084563bffcdff365dd55a00
Arturo Gonzalez <arturogf93@gmail.com>
2022-03-13 11:55:59 -0600

    meta test

---
+++ pages/pokemon.vue
@@ -0,0 +1,16 @@
+<template>
+	<Html>
+		<Head>
+			<Title>{{ dynamic }} title</Title>
+			<Meta name="description" :content="`My page's ${dynamic} description`" />
+			<Link rel="preload" href="/test.txt" as="script" />
+			<Style type="text/css" :children="styleString" />
+		</Head>
+	</Html>
+	<div></div>
+</template>
+<script>
+export default {
+  data: () => ({ dynamic: 49, styleString: 'body { background-color: green; }' })
+}
+</script>
