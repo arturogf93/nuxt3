452e17c480cc62f36dd427d140085eaaf9c7dbf9
Arturo Gonzalez <arturogf93@gmail.com>
2022-03-12 22:12:48 -0600

    remove primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,6 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
 	css: [
-		'primevue/resources/themes/lara-light-blue/theme.css',
-		'primevue/resources/primevue.min.css',
-		'primeicons/primeicons.css',
-		'primeflex/primeflex.css',
 		'@/assets/css/style.css'
 ]
 })
